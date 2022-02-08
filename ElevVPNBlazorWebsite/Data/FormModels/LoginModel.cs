6c7b4b0097c33648109317d7b573a815047984e7
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-02-08 11:54:29 +0100

    Login page Implementation

--- ElevVPNBlazorWebsite/Data/FormModels/LoginModel.cs
+++ ElevVPNBlazorWebsite/Data/FormModels/LoginModel.cs
@@ -2,11 +2,15 @@
 
 namespace ElevVPNBlazorWebsite.Data.FormModels
 {
-    public class RequestModel : BaseModel
+    public class LoginModel : BaseModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Email adresse er påkrævet.")]
         [StringLength(255, ErrorMessage = "Email er for lang (255 karakter begrænse.).")]
         [EmailAddress(ErrorMessage = "Indtast en gyldig email adresse.")]
         public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Adgangskode er påkrævet.")]
+        [StringLength(255)]
+        public string Password { get; set; }
     }
 }
--- ElevVPNBlazorWebsite/Pages/Dashboard/LoginPage.razor.cs
+++ ElevVPNBlazorWebsite/Pages/Dashboard/LoginPage.razor.cs
@@ -1,6 +1,19 @@
-﻿namespace ElevVPNBlazorWebsite.Pages.Dashboard
+﻿using ElevVPNBlazorWebsite.Data.FormModels;
+using System.Threading.Tasks;
+
+namespace ElevVPNBlazorWebsite.Pages.Dashboard
 {
     public partial class LoginPage
     {
+        private LoginModel _loginModel = new();
+
+        private Task OnValidSubmit_LoginAdmin()
+        {
+            _loginModel.IsProccessing = true;
+
+
+            _loginModel.IsProccessing = false;
+            return Task.CompletedTask;
+        }
     }
 }
