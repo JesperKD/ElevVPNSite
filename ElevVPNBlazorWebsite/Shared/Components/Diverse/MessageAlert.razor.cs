6a6c5fea05f467a4edc6ec736c678e36558ead00
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-03-01 14:47:58 +0100

    MessageAlert Component

---
+++ ElevVPNBlazorWebsite/Shared/Components/Diverse/MessageAlert.razor.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace ElevVPNBlazorWebsite.Shared.Components.Diverse
+{
+    public partial class MessageAlert : ComponentBase
+    {
+        [Parameter] public string Message { get; set; }
+        [Parameter] public string MessageOptional { get; set; } = string.Empty;
+        [Parameter] public AlertType Alert { get; set; }
+        [Parameter] public bool IsLoading { get; set; }
+        [Parameter] public bool FullWidth { get; set; }
+
+        public string Container
+        {
+            get
+            {
+                if (FullWidth)
+                {
+                    return "container-fluid";
+                }
+                else
+                {
+                    return "container";
+                }
+            }
+        }
+
+
+        protected override Task OnParametersSetAsync()
+        {
+            if (string.IsNullOrEmpty(Message))
+            {
+                return Task.CompletedTask;
+            }
+
+            var x = Task.Delay(10000)
+                .ContinueWith(x =>
+                {
+                    if (string.IsNullOrEmpty(Message))
+                    {
+                        return x.IsCompleted;
+                    }
+                    Message = "";
+                    OnMessageCleared(true);
+                    return x.IsCompleted;
+                });
+
+            return x;
+        }
+        public event EventHandler<bool> MessageCleared;
+        protected virtual void OnMessageCleared(bool result)
+        {
+            EventHandler<bool> handler = MessageCleared;
+            if (handler is not null)
+            {
+                handler(this, result);
+            }
+        }
+
+        public enum AlertType { Info, Warning, Error, Success }
+    }
+}
