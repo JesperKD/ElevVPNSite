8882689d2c786592a942e1f3cc130e49b3321c2b
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-03-01 15:39:59 +0100

    Errormessage update

--- ElevVPNBlazorWebsite/Data/FormModels/LoginModel.cs
+++ ElevVPNBlazorWebsite/Data/FormModels/LoginModel.cs
@@ -6,7 +6,7 @@ namespace ElevVPNBlazorWebsite.Data.FormModels
     public class LoginModel : BaseModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Email adresse er påkrævet.")]
-        [StringLength(255, ErrorMessage = "Email er for lang (255 karakter begrænse.).")]
+        [StringLength(255, ErrorMessage = "Email er for lang (255 karakter max).")]
         [EmailAddress(ErrorMessage = "Indtast en gyldig email adresse.")]
         public string Email { get; set; }
 
