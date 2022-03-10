d34146008a6675f04df1cb958211e238981f9462
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-03-10 16:01:37 +0100

    Login messagealert update

--- ElevVPNBlazorWebsite/Pages/Dashboard/LoginPage.razor.cs
+++ ElevVPNBlazorWebsite/Pages/Dashboard/LoginPage.razor.cs
@@ -1,19 +1,33 @@
 ﻿using ElevVPNBlazorWebsite.Data.FormModels;
+using ElevVPNClassLibrary.Common.Users.Services;
+using Microsoft.AspNetCore.Components;
+using System;
 using System.Threading.Tasks;
 
 namespace ElevVPNBlazorWebsite.Pages.Dashboard
 {
     public partial class LoginPage
     {
+        [Inject] protected UserService UserService { get; set; }
         private readonly LoginModel _loginModel = new();
 
-        private Task OnValidSubmit_LoginAdmin()
+        private string _errorMessage = string.Empty;
+        private string _successMessage = string.Empty;
+
+        private async Task OnValidSubmit_LoginAdmin()
         {
             _loginModel.IsProcessing = true;
 
-
+            try
+            {
+                await UserService.AuthenticateUserLoginAsync(_loginModel.Email, _loginModel.Password);
+            }
+            catch (Exception ex)
+            {
+                _errorMessage = "Kunne ikke logge ind, prøv igen.";
+                Console.WriteLine("Login error: " + ex.Message);
+            }
             _loginModel.IsProcessing = false;
-            return Task.CompletedTask;
         }
     }
 }
