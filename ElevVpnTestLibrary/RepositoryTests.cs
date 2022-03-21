303a829551b30bd38796a897664ece8b82b98c1c
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-03-21 08:33:19 +0100

    Update RepositoryTests.cs

--- ElevVpnTestLibrary/RepositoryTests.cs
+++ ElevVpnTestLibrary/RepositoryTests.cs
@@ -3,6 +3,7 @@ using ElevVPNClassLibrary.Common.Users.Factories;
 using ElevVPNClassLibrary.Common.Users.Repositories;
 using ElevVpnTestLibrary.Setup;
 using NUnit.Framework;
+using System;
 using System.Threading.Tasks;
 
 namespace ElevVpnTestLibrary
@@ -36,6 +37,41 @@ namespace ElevVpnTestLibrary
             Assert.AreNotEqual(0, firstUser.Id);
             Assert.IsNotNull(firstUser.Email);
             Assert.IsNotEmpty(firstUser.Email);
+        }
+
+        [Test]
+        [Order(1)]
+        public async Task CreateAsync_CreatesAnArea_IfArgumentsAreValid()
+        {
+            //Arrange
+            IUser expected;
+            IUser actual;
+
+            expected = CreateTestAreaObject();
+
+            //Act
+            actual = await _userRepository.CreateAsync(expected);
+            bool cleanUpAreaTest = await _userRepository.RemoveAsync(actual);
+
+            //Assert
+            Assert.IsNotNull(actual);
+            Assert.IsNotEmpty(actual.Email);
+            Assert.AreNotEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.IsTrue(cleanUpAreaTest);
+        }
+
+        private static IUser CreateTestAreaObject()
+        {
+            IUser expected;
+            var rnd = new Random(DateTime.Now.GetHashCode());
+            string testUserMail = $"UnitTest{rnd.Next(3424, 458729)}";
+            expected = new User
+            {
+                Id = 0,
+                Email = testUserMail
+            };
+            return expected;
         }
     }
 }
